31d8837a0c7d8359ac944aa6cdd80d92133d0170
jerem <jeremy.timperio@anduin.ai>
2021-05-25 13:04:08 -0400

    updated package scripts

--- front-end/fantasy-app/server.js
+++ front-end/fantasy-app/server.js
@@ -1,9 +1,16 @@
-// server.js
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
