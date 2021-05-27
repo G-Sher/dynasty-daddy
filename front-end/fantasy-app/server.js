d73aed258a90eafeed8b8eb1c5055a9950628191
jerem <jeremy.timperio@anduin.ai>
2021-05-27 12:31:32 -0400

    updated deployment script for front end

--- front-end/fantasy-app/server.js
+++ front-end/fantasy-app/server.js
@@ -5,12 +5,25 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fantasy-app'));
+app.use(express.static(__dirname + '/dist/dynasty'));
 
-app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/fantasy-app/index.html'));
+const allowed = [
+  '.js',
+  '.css',
+  '.png',
+  '.jpg'
+];
+
+// Catch all other routes and return the angular index file
+app.get('*', (req, res) => {
+  if (allowed.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
+    res.sendFile(path.resolve(`dist/${req.url}`));
+  } else {
+    res.sendFile(path.join(__dirname, 'dist/dynasty/index.html'));
+  }
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
+
